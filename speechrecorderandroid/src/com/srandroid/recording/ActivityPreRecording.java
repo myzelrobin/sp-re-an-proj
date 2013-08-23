bc7c822b60ee1db5027ad9c0ae6e17a5c1bddeb5
flc <flc@flc-asus>
2013-08-24 01:51:00 +0200

    test

--- speechrecorderandroid/src/com/srandroid/recording/ActivityPreRecording.java
+++ speechrecorderandroid/src/com/srandroid/recording/ActivityPreRecording.java
@@ -41,17 +41,22 @@ public class ActivityPreRecording extends Activity
 	
 	private GridView gridView = null;
 	
-	private TextView name = null;
-	private TextView accent = null;
-	private TextView sex = null;
-	private TextView birthday = null;
-	private TextView sessions1 = null;
-	private TextView scripts1 = null;
+
+	// speaker item
+	private TextView name = (TextView) findViewById(R.id.act_prerecording_speakeritem_name_textvalue);
+	private TextView accent = (TextView) findViewById(R.id.act_prerecording_speakeritem_accent_textvalue);
+	private TextView sex = (TextView) findViewById(R.id.act_prerecording_speakeritem_sex_textvalue);
+	private TextView birthday = (TextView) findViewById(R.id.act_prerecording_speakeritem_birthday_textvalue);
+	private TextView sessions1 = (TextView) findViewById(R.id.act_prerecording_speakeritem_sessions_textvalue);
+	private TextView scripts1 = (TextView) findViewById(R.id.act_prerecording_speakeritem_scripts_textvalue);
 	
-	private TextView scriptid = null;
-	private TextView scriptdesc = null;
-	private TextView sessions2 = null;
-	private TextView speakers2 = null;
+	
+	// script item
+	private TextView scriptid = (TextView) findViewById(R.id.act_prerecording_scriptitem_scriptid_textvalue);
+	private TextView scriptdesc = (TextView) findViewById(R.id.act_prerecording_scriptitem_desc_textvalue);
+	private TextView sessions2 = (TextView) findViewById(R.id.act_prerecording_scriptitem_sessions_textvalue);
+	private TextView speakers2 = (TextView) findViewById(R.id.act_prerecording_scriptitem_speakers_textvalue);
+	
 
 	/**
 	 * 
@@ -83,24 +88,8 @@ public class ActivityPreRecording extends Activity
         
         setContentView(R.layout.gridviewlayout_act_prerecording);
         
-        gridView = (GridView) findViewById(R.id.id_gridview_act_prerecoding);
         
-		
-		// speaker item
-		name = (TextView) findViewById(R.id.act_prerecording_speakeritem_name_textvalue);
-		accent = (TextView) findViewById(R.id.act_prerecording_speakeritem_accent_textvalue);
-		sex = (TextView) findViewById(R.id.act_prerecording_speakeritem_sex_textvalue);
-		birthday = (TextView) findViewById(R.id.act_prerecording_speakeritem_birthday_textvalue);
-		sessions1 = (TextView) findViewById(R.id.act_prerecording_speakeritem_sessions_textvalue);
-		scripts1 = (TextView) findViewById(R.id.act_prerecording_speakeritem_scripts_textvalue);
-		
-		
-		// script item
-        scriptid = (TextView) findViewById(R.id.act_prerecording_scriptitem_scriptid_textvalue);
-        scriptdesc = (TextView) findViewById(R.id.act_prerecording_scriptitem_desc_textvalue);
-        sessions2 = (TextView) findViewById(R.id.act_prerecording_scriptitem_sessions_textvalue);
-        speakers2 = (TextView) findViewById(R.id.act_prerecording_scriptitem_speakers_textvalue);
-
+        gridView = (GridView) findViewById(R.id.id_gridview_act_prerecoding);
         
         
         
